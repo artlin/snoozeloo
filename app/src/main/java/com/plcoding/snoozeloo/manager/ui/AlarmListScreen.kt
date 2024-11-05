bf5e3844933fe005e47ca5ec3750d53f4bae309c
Michal <michal.rabinski@vestiacom.com>
2024-11-05 21:19:07 +0100

    Added two empty screens for manager

---
+++ app/src/main/java/com/plcoding/snoozeloo/manager/ui/AlarmListScreen.kt
@@ -0,0 +1,14 @@
+package com.plcoding.snoozeloo.manager.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AlarmList(){
+    Box(Modifier.background(Color.Green).size(100.dp))
+}
---
+++ app/src/main/java/com/plcoding/snoozeloo/manager/ui/EditAlarmScreen.kt
@@ -0,0 +1,14 @@
+package com.plcoding.snoozeloo.manager.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EditAlarm() {
+    Box(Modifier.background(Color.Red).size(100.dp))
+}

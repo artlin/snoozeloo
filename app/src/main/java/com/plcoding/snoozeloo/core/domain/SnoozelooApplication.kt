8d1eee9f14d1b253f101dcf809594185c4c969fd
Artur Skrzypczyk <skrzypczyk.artur@gmail.com>
2024-11-06 08:20:07 +0100

    different koin initialization

--- app/src/main/java/com/plcoding/snoozeloo/core/domain/SnoozelooApplication.kt
+++ app/src/main/java/com/plcoding/snoozeloo/core/domain/SnoozelooApplication.kt
@@ -4,21 +4,20 @@ import android.app.Application
 import com.plcoding.snoozeloo.di.coreModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androix.startup.KoinStartup.onKoinStartup
+import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class SnoozelooApplication: Application() {
 
-    init {
-        onKoinStartup {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
             androidLogger(Level.ERROR)
             androidContext(this@SnoozelooApplication)
             modules(coreModule)
-        }
-    }
 
-    override fun onCreate() {
-        super.onCreate()
+        }
     }
 
 }

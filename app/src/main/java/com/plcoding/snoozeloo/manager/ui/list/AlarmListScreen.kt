7df3b9e0fa412d01383ec0d61e47f94037772a15
Michal <michal.rabinski@vestiacom.com>
2024-11-07 22:42:17 +0100

    partially added empty screen

--- app/src/main/java/com/plcoding/snoozeloo/manager/ui/list/AlarmListScreen.kt
+++ app/src/main/java/com/plcoding/snoozeloo/manager/ui/list/AlarmListScreen.kt
@@ -2,13 +2,19 @@ package com.plcoding.snoozeloo.manager.ui.list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.plcoding.snoozeloo.R
 import com.plcoding.snoozeloo.core.ui.text.TextTitle1
 import com.plcoding.snoozeloo.core.ui.text.TextTitle2
 import com.plcoding.snoozeloo.navigation.NavigationController
@@ -20,6 +26,7 @@ fun AlarmListScreen() {
     // todo : only for testing purpose, navigation controller should be moved into viewModel
     val navigation = koinInject<NavigationController>()
     AlarmListHeader()
+    EmptyScreen()
 }
 
 @Composable
@@ -27,3 +34,22 @@ fun AlarmListHeader() {
     TextTitle1("Your alarms", Color(0xFF0D0F19))
 }
 
+@Composable
+fun EmptyScreen() {
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(35.dp, alignment = Alignment.CenterVertically)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.icon_alarm),
+            contentDescription = null,
+            tint = Color(0xFF4664FF)
+
+        )
+        TextTitle2(
+            "It's empty! Add the first alarm so you don't miss an important moment!",
+            color = Color(0xFF0D0F19)
+        )
+    }
+}

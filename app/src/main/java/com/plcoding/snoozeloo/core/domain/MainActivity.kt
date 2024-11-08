27f3fa2436040d897eefecdda846a2836b887056
Michal <michal.rabinski@vestiacom.com>
2024-11-08 13:52:05 +0100

    removed unnecessary dispose effect

--- app/src/main/java/com/plcoding/snoozeloo/core/domain/MainActivity.kt
+++ app/src/main/java/com/plcoding/snoozeloo/core/domain/MainActivity.kt
@@ -5,12 +5,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.plcoding.snoozeloo.core.ui.FAB
 import com.plcoding.snoozeloo.core.ui.theme.SnoozelooTheme
 import com.plcoding.snoozeloo.navigation.NavigationController
 import com.plcoding.snoozeloo.navigation.NavigationControllerImpl
@@ -35,13 +32,9 @@ class MainActivity : ComponentActivity() {
                     Log.w("TAG", "MainActivity doSomething")
                     // navigation
                     val navHostController = rememberNavController()
-                    DisposableEffect(navHostController) {
-                        (navController as? NavigationControllerImpl)?.setNavController(
-                            navHostController
-                        )
-                        onDispose {
-                        }
-                    }
+                    (navController as? NavigationControllerImpl)?.setNavController(
+                        navHostController
+                    )
                     RootGraph(navController = navHostController)
                 }
             }

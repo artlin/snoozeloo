1b2b7bcf24b8aea15a45a60367022b61b7e6dca5
Artur Skrzypczyk <skrzypczyk.artur@gmail.com>
2025-01-04 04:09:38 +0100

    moving scheduleAlarm to IO thread

--- app/src/main/java/com/plcoding/snoozeloo/scheduler/AlarmScheduler.kt
+++ app/src/main/java/com/plcoding/snoozeloo/scheduler/AlarmScheduler.kt
@@ -3,7 +3,7 @@ package com.plcoding.snoozeloo.scheduler
 import com.plcoding.snoozeloo.core.domain.db.Alarm
 
 interface AlarmScheduler {
-    fun scheduleAlarm(alarm: Alarm)
+    suspend fun scheduleAlarm(alarm: Alarm)
     fun cancelAlarm(alarm: Alarm)
     fun cancelAllAlarms()
 }
--- app/src/main/java/com/plcoding/snoozeloo/scheduler/AlarmSchedulerImpl.kt
+++ app/src/main/java/com/plcoding/snoozeloo/scheduler/AlarmSchedulerImpl.kt
@@ -1,18 +1,24 @@
 package com.plcoding.snoozeloo.scheduler
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import com.plcoding.snoozeloo.core.domain.db.Alarm
 import com.plcoding.snoozeloo.core.domain.findNextScheduleTimeEpochSeconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class AlarmSchedulerImpl(
     private val context: Context,
 ): AlarmScheduler {
 
     private val alarmManager = context.getSystemService(AlarmManager::class.java)
-    override fun scheduleAlarm(alarm: Alarm) {
+
+    @SuppressLint("MissingPermission")
+    override suspend fun scheduleAlarm(alarm: Alarm) {
+        withContext(Dispatchers.IO) {
         val intent = Intent(context, AlarmReceiver::class.java).apply {
             putExtra("ALARM_ID", alarm.id)
         }
@@ -20,7 +26,7 @@ class AlarmSchedulerImpl(
 
         println("Alarm scheduled at ${alarm.hours} hours and ${alarm.minutes} minutes")
 
-        val nextAlarmOccurence = findNextScheduleTimeEpochSeconds(alarm.hours, alarm.minutes) * 1000L
+        val nextAlarmOccurrence = findNextScheduleTimeEpochSeconds(alarm.hours, alarm.minutes) * 1000L
 
         val pendingIntent = PendingIntent.getBroadcast(
             context,
@@ -30,7 +36,7 @@ class AlarmSchedulerImpl(
         )
 
         val alarmInfo = AlarmManager.AlarmClockInfo(
-            nextAlarmOccurence,
+            nextAlarmOccurrence,
             pendingIntent
         )
 
@@ -49,6 +55,7 @@ class AlarmSchedulerImpl(
 //                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
 //            )
 //        )
+        }
     }
 
 //    override fun cancelAlarm(item: AlarmItem) {
